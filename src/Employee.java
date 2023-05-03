f147fb1e341c172ee9a6b8d0dab2026b7e9992f8
Aleksandra Trimailova <aleksandratrimailova@gmail.com>
2023-05-03 23:17:28 +0200

    adding fields to the class created constructor "Employee" create method "toString"

--- src/Employee.java
+++ src/Employee.java
@@ -1,2 +1,21 @@
 public class Employee {
+    String firstName;
+    String secondName;
+    int experience;
+    String jobTitle;
+    double moneyAccount;
+    Employee(String firstName, String secondName, int experience, String jobTitle, double moneyAccount){
+        this.firstName = firstName;
+        this.secondName = secondName;
+        this.experience = experience;
+        this.jobTitle = jobTitle;
+        this.moneyAccount = moneyAccount;
+    }
+    public String toString (){
+        return "имя: " + firstName +
+                " фамилия:" + secondName +
+                " стаж: " + experience +
+                " дожность: " + jobTitle +
+                " денежный счет: " + moneyAccount;
+    }
 }
